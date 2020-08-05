4cb1938c6b60b11f58ce5c499fdfda86ebaa693c
FahimHassanMollah <fahimhassan0197@gmail.com>
2020-08-05 15:54:08 +0600

    project finished digital clock

---
+++ app.js
@@ -0,0 +1,24 @@
+function digitalClock() {
+
+    let date=new Date();
+    let hours=date.getHours();
+    let minutes=date.getMinutes();
+    let seconds=date.getSeconds();
+    let amPm='AM'
+    if (hours===0){
+        hours=12;
+    }
+    if (hours>12){
+        hours=hours-12;
+        amPm='PM'
+
+    }
+    hours=hours<10?`0${hours}`:hours;
+    minutes=minutes<10?`0${minutes}`:minutes;
+    seconds=seconds<10?`0${seconds}`:seconds;
+    document.getElementById('time').innerText=`${hours}:${minutes}:${seconds}`
+    document.getElementById('am-pm').innerText=amPm;
+    setInterval(digitalClock,1000);
+   
+}
+digitalClock();
